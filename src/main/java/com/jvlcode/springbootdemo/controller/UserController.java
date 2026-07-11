fb577af730c4a1ac5e2537231faf9c3dbb3caddf
Madhankannan02 <167090440+Madhankannan02@users.noreply.github.com>
2026-07-11 20:11:53 +0530

    feat: createUser method have been added

--- src/main/java/com/jvlcode/springbootdemo/controller/UserController.java
+++ src/main/java/com/jvlcode/springbootdemo/controller/UserController.java
@@ -5,11 +5,9 @@ import java.util.*;
 import com.jvlcode.springbootdemo.entity.UserEntity;
 import com.jvlcode.springbootdemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.jvlcode.springbootdemo.model.User;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
@@ -21,5 +19,10 @@ public class UserController {
     @GetMapping
     public List<UserEntity> getUsers() {
         return userRepository.findAll();
+    }
+
+    @PostMapping
+    public UserEntity createUser(@RequestBody UserEntity user) {
+        return userRepository.save(user);
     }
 }
