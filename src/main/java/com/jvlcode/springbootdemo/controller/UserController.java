8c2289f1d93a8c4d3dce6d1507ae8d4fecbb7201
Madhankannan02 <167090440+Madhankannan02@users.noreply.github.com>
2026-07-11 20:03:08 +0530

    feat: UserRespository created and is returning the table data

--- src/main/java/com/jvlcode/springbootdemo/controller/UserController.java
+++ src/main/java/com/jvlcode/springbootdemo/controller/UserController.java
@@ -2,7 +2,9 @@ package com.jvlcode.springbootdemo.controller;
 
 import java.util.*;
 
+import com.jvlcode.springbootdemo.entity.UserEntity;
 import com.jvlcode.springbootdemo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,7 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping
-    public
+    public List<UserEntity> getUsers() {
+        return userRepository.findAll();
+    }
 }
--- src/main/java/com/jvlcode/springbootdemo/entity/UserEntity.java
+++ src/main/java/com/jvlcode/springbootdemo/entity/UserEntity.java
@@ -17,6 +17,30 @@ public class UserEntity {
         this.email = email;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public UserEntity() {
         super();
     }
