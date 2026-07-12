6c7c40fe98408e60e4732c68a4cab87348b6bcf6
Madhankannan02 <167090440+Madhankannan02@users.noreply.github.com>
2026-07-12 19:10:17 +0530

    feat: Delete User API have been created

--- src/main/java/com/jvlcode/springbootdemo/controller/UserController.java
+++ src/main/java/com/jvlcode/springbootdemo/controller/UserController.java
@@ -6,9 +6,12 @@ import com.jvlcode.springbootdemo.entity.UserEntity;
 import com.jvlcode.springbootdemo.exception.ResourceNotFoundException;
 import com.jvlcode.springbootdemo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.jvlcode.springbootdemo.model.User;
+
+import static com.jvlcode.springbootdemo.repository.UserRepository.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -30,5 +33,19 @@ public class UserController {
     @GetMapping("/{id}")
     public UserEntity getUserById(@PathVariable Long id) {
        return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User Not Found with this id:" + id));
+    }
+
+    @PutMapping("/{id}")
+    public UserEntity updateUser(@RequestBody UserEntity user, @PathVariable Long id) {
+        UserEntity userData = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User Not Found"));
+        userData.setName(user.getName());
+        return userRepository.save(userData);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+        UserEntity userData = userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User Not Found"));
+        userRepository.delete(userData);
+        return ResponseEntity.ok().build();
     }
 }
