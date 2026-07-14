4638b691988cbecdb0216caf90794b1b1f15f254
Madhankannan02 <167090440+Madhankannan02@users.noreply.github.com>
2026-07-14 20:48:04 +0530

    feat: Made the securityFilterChain as @Bean

--- src/main/java/com/jvlcode/springbootdemo/config/SecurityConfig.java
+++ src/main/java/com/jvlcode/springbootdemo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.jvlcode.springbootdemo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,9 +9,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http.authorizeHttpRequests(authz ->
             authz.requestMatchers("/api/users/**").authenticated()
+                    .requestMatchers("/home").permitAll()
         );
 
         return http.build();
