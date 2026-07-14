8997e7af1081ae5de0e4d4f7150e9b1db886b942
Madhankannan02 <167090440+Madhankannan02@users.noreply.github.com>
2026-07-14 20:31:38 +0530

    feat: Created HomeController.java to return the homepage content

--- src/main/java/com/jvlcode/springbootdemo/controller/HomeController.java
+++ src/main/java/com/jvlcode/springbootdemo/controller/HomeController.java
@@ -1,4 +1,15 @@
 package com.jvlcode.springbootdemo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
 public class HomeController {
+
+    @GetMapping("/home")
+    public String getHomePage() {
+        return "Welcome to home page";
+    }
 }
